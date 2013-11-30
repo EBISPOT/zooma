981f6ad9ac13d6c51bbfc2355fcd118bf14c9750
Tony Burdett <tburdett@ebi.ac.uk>
2013-11-30 17:41:37 +0000

    implementing needleman-wunsch and jaccard similarity to obtain string similarity metric as part of overall zooma score

--- zooma-impl/src/main/java/uk/ac/ebi/fgpt/zooma/util/AnnotationSummaryScorer.java
+++ zooma-impl/src/main/java/uk/ac/ebi/fgpt/zooma/util/AnnotationSummaryScorer.java
@@ -1,10 +1,11 @@
 package uk.ac.ebi.fgpt.zooma.util;
 
 import uk.ac.ebi.fgpt.zooma.model.AnnotationSummary;
+import uk.ac.shef.wit.simmetrics.similaritymetrics.JaccardSimilarity;
+import uk.ac.shef.wit.simmetrics.similaritymetrics.NeedlemanWunch;
 
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 
 /**
@@ -15,6 +16,14 @@ import java.util.Map;
  * @date 30/11/13
  */
 public class AnnotationSummaryScorer extends AbstractQualityBasedScorer<AnnotationSummary> {
+    private NeedlemanWunch nwSimilarity;
+    private JaccardSimilarity jaccardSimilarity;
+
+    public AnnotationSummaryScorer() {
+        this.nwSimilarity = new NeedlemanWunch();
+        this.jaccardSimilarity = new JaccardSimilarity();
+    }
+
     @Override
     public Map<AnnotationSummary, Float> score(Collection<AnnotationSummary> collection, String searchString) {
         Map<AnnotationSummary, Float> results = new HashMap<>();
@@ -33,26 +42,9 @@ public class AnnotationSummaryScorer extends AbstractQualityBasedScorer<Annotati
     }
 
     private float getSimilarity(String s1, String s2) {
-        // todo - implement this to consider string similarity using appropriate algorithm
-        return 1.0f;
-
-        // possible scoring implementation...
-//        for (AnnotationSummary as : modifiedResults.keySet()) {
-//            if (results.containsKey(as)) {
-//                // results already contains this result
-//                float previousScore = results.get(as);
-//                // so calculate the weight of the lexical score based on similarity
-//                float newScore = modifiedResults.get(as) * similarStrings.get(s) * similarStrings.get(s);
-//
-//                if (newScore > previousScore) {
-//                    // if the lexical score is higher than the zooma score, override zooma result
-//                    results.put(as, newScore);
-//                }
-//            }
-//            else {
-//                // add the result, and weight the lexical score based on similarity
-//                results.put(as, modifiedResults.get(as) * similarStrings.get(s) * similarStrings.get(s));
-//            }
-//        }
+        // similarity is average NW and Jaccard score, squared
+        float nw = nwSimilarity.getSimilarity(s1, s2);
+        float jaccard = jaccardSimilarity.getSimilarity(s1, s2);
+        return (float) Math.pow((double) (nw + jaccard) / 2, 2);
     }
 }
