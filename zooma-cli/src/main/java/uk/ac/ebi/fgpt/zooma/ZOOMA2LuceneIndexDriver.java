33f7ae74c2bd30b22a5ee5dfdd2ca8b96e79e6a2
Tony Burdett <tburdett@ebi.ac.uk>
2013-12-11 16:20:10 +0000

    recursive delete in CLI scripts if backup already exists

--- zooma-cli/src/main/java/uk/ac/ebi/fgpt/zooma/ZOOMA2LuceneIndexDriver.java
+++ zooma-cli/src/main/java/uk/ac/ebi/fgpt/zooma/ZOOMA2LuceneIndexDriver.java
@@ -12,9 +12,12 @@ import uk.ac.ebi.fgpt.zooma.service.StatusService;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -181,6 +184,19 @@ public class ZOOMA2LuceneIndexDriver {
                     else {
                         System.out.print(
                                 "Backup already exists for today, clearing " + oldZoomaHome.toString() + "...");
+                        Files.walkFileTree(oldZoomaHome, new SimpleFileVisitor<Path>() {
+                            @Override public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                                    throws IOException {
+                                Files.delete(file);
+                                return FileVisitResult.CONTINUE;
+                            }
+
+                            @Override public FileVisitResult postVisitDirectory(Path dir, IOException exc)
+                                    throws IOException {
+                                Files.delete(dir);
+                                return FileVisitResult.CONTINUE;
+                            }
+                        });
                         Files.delete(oldZoomaHome);
                         System.out.println("ok!");
                     }
