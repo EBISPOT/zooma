fa2b5108b4ea72cf417663fb170cd9f287605347
Tony Burdett <tburdett@ebi.ac.uk>
2013-12-17 17:59:33 +0000

    refining interfaces for annotation source retrieval

--- zooma-api/src/main/java/uk/ac/ebi/fgpt/zooma/datasource/AnnotationSourceDAO.java
+++ zooma-api/src/main/java/uk/ac/ebi/fgpt/zooma/datasource/AnnotationSourceDAO.java
@@ -3,7 +3,8 @@ package uk.ac.ebi.fgpt.zooma.datasource;
 import uk.ac.ebi.fgpt.zooma.model.AnnotationSource;
 
 /**
- * Javadocs go here!
+ * A data access object that defines methods to create, retrieve, update and delete annotation sources from a ZOOMA
+ * datasource.
  *
  * @author Tony Burdett
  * @date 17/12/13
