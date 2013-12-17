ede49a4660e49a16393fc98a3faed9fcfa260c94
Tony Burdett <tburdett@ebi.ac.uk>
2013-12-17 17:41:09 +0000

    adding annotation source dao support

---
+++ zooma-api/src/main/java/uk/ac/ebi/fgpt/zooma/datasource/AnnotationSourceDAO.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.fgpt.zooma.datasource;
+
+import uk.ac.ebi.fgpt.zooma.model.AnnotationSource;
+
+/**
+ * Javadocs go here!
+ *
+ * @author Tony Burdett
+ * @date 17/12/13
+ */
+public interface AnnotationSourceDAO extends ZoomaDAO<AnnotationSource> {
+}
