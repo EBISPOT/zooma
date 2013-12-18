a610242ae1925f2a629cd5eb46003553896021f9
Tony Burdett <tburdett@ebi.ac.uk>
2013-12-18 15:09:01 +0000

    updating link to atlas logo

--- zooma-ui/src/main/webapp/scripts/zooma-ui.js
+++ zooma-ui/src/main/webapp/scripts/zooma-ui.js
@@ -341,7 +341,7 @@ function renderResults(data) {
                                     encodeURIComponent(result[1]) +
                                     "&view=hm&searchMode=simple";
                     row = row + "<td><a href='" + href + "' target='_blank'>" +
-                            "<img src='http://www.ebi.ac.uk/gxa/images/ExpressionAtlas_logo_web.png' " +
+                            "<img src='http://www.ebi.ac.uk/gxa/resources/images/ExpressionAtlas_logo_web.png' " +
                             "alt='Expression Atlas' style='height: 22px;'/> Expression Atlas</a></td>";
                 }
                 else if (result[7] == "http://www.ebi.ac.uk/arrayexpress") {
