e90cb3a5c5755252bc5646fc780251fd25a52819
Tony Burdett <tburdett@ebi.ac.uk>
2014-06-19 16:33:13 +0100

    massive refactoring to migrate all configuration into one place and remove config dependencies from each module.  Also some separation of search/build command line apps

--- zooma-reporting/src/main/java/uk/ac/ebi/fgpt/zooma/io/ZOOMAReportRenderer.java
+++ zooma-reporting/src/main/java/uk/ac/ebi/fgpt/zooma/io/ZOOMAReportRenderer.java
@@ -187,6 +187,11 @@ public class ZOOMAReportRenderer {
     }
 
     public void renderAnnotationSummaries(List<Property> properties,
+                                          Map<Property, Map<AnnotationSummary, Float>> propertySummariesMap) {
+        renderAnnotationSummaries(properties, Collections.<Property, List<String>>emptyMap(), propertySummariesMap);
+    }
+
+    public void renderAnnotationSummaries(List<Property> properties,
                                           Map<Property, List<String>> propertyContextMap,
                                           Map<Property, Map<AnnotationSummary, Float>> propertySummariesMap) {
         // first, write the report header
@@ -233,7 +238,7 @@ public class ZOOMAReportRenderer {
 
                 for (AnnotationSummary summary : reportList) {
                     if (propertyContextMap.containsKey(property)) {
-                        // render one line per posible summary
+                        // render one line per possible summary
                         for (URI annotatesTo : summary.getSemanticTags()) {
                             if (annotatesTo != null) {
                                 writeEvaluationLine(writer,
@@ -281,7 +286,7 @@ public class ZOOMAReportRenderer {
 
     protected void writeReportLine(PrintWriter writer,
                                    Property property,
-                                   String experiment,
+                                   String study,
                                    String source,
                                    Collection<URI> semanticTags,
                                    boolean automatic) {
@@ -303,7 +308,7 @@ public class ZOOMAReportRenderer {
         StringBuilder synonymsSB = new StringBuilder();
         StringBuilder ontologiesSB = new StringBuilder();
         Iterator<URI> semanticTagIterator = semanticTags.iterator();
-        while(semanticTagIterator.hasNext()) {
+        while (semanticTagIterator.hasNext()) {
             URI semanticTag = semanticTagIterator.next();
 
             // use fragment as 'term'
@@ -341,12 +346,12 @@ public class ZOOMAReportRenderer {
         String ontologies = ontologiesSB.toString();
         writer.println(propertyType + "\t" + propertyValue + "\t" + labels + "\t" +
                                synonyms + "\t" + type + "\t" + terms + "\t" +
-                               ontologies + "\t" + source);
+                               ontologies + "\t" + source + "\t" + study);
     }
 
     protected void writeUnmappedReportLine(PrintWriter writer,
                                            Property property,
-                                           String experiment) {
+                                           String study) {
         getLog().trace("There are no mappings for property '" + property + "'");
 
         String propertyType =
@@ -362,7 +367,7 @@ public class ZOOMAReportRenderer {
 
         writer.println(propertyType + "\t" + propertyValue + "\t" + labels + "\t" +
                                synonyms + "\t" + type + "\t" + terms + "\t" +
-                               ontologies + "\t" + sources);
+                               ontologies + "\t" + sources + "\t" + study);
     }
 
     protected void writeReportHeader(PrintWriter writer) {
@@ -375,7 +380,7 @@ public class ZOOMAReportRenderer {
         writer.flush();
 
         writer.println(
-                "PROPERTY TYPE\tPROPERTY VALUE\tONTOLOGY TERM LABEL(S)\tONTOLOGY TERM SYNONYM(S)\tMAPPING TYPE\tONTOLOGY TERM(S)\tONTOLOGY(S)\tSOURCE(S)");
+                "PROPERTY TYPE\tPROPERTY VALUE\tONTOLOGY TERM LABEL(S)\tONTOLOGY TERM SYNONYM(S)\tMAPPING TYPE\tONTOLOGY TERM(S)\tONTOLOGY(S)\tSOURCE(S)\tSTUDY");
     }
 
     protected void writeEvaluationLine(PrintWriter writer,
