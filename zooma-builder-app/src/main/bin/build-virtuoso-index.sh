e3fbc982f526bdeb309a9fa69c42dc91a7bf6555
Tony Burdett <tburdett@ebi.ac.uk>
2014-06-25 12:48:36 +0100

    final cleanup of virtuoso consolidated build scripts

--- zooma-builder-app/src/main/bin/build-virtuoso-index.sh
+++ zooma-builder-app/src/main/bin/build-virtuoso-index.sh
@@ -10,20 +10,20 @@ usage() {
 
 loadProperty()
 {
-    local key="$1"
-    local target=$zoomaHome/config/zooma.properties
+    local key="$1";
+    local target=$zoomaHome/config/zooma.properties;
     cat ${target} | sed -e '/^\#/d' | sed -e '/^\s*$/d' | sed -e 's/\s\+/=/g' | grep ${key} |
         while read LINE
         do
-            local KEY=`echo ${LINE} | cut -d "=" -f 1`
-            local VALUE=`echo ${LINE} | cut -d "=" -f 2`
+            local KEY=`echo ${LINE} | cut -d "=" -f 1`;
+            local VALUE=`echo ${LINE} | cut -d "=" -f 2`;
             [ ${key} == ${KEY} ] && {
                 local UNKNOWN_NAME=`echo $VALUE | grep '\${.*}' -o | sed 's/\${//' | sed 's/}//'`
                 if [ ! $UNKNOWN_NAME ];
                 then
-                    echo ${VALUE}
+                    echo ${VALUE};
                 fi
-                return
+                return;
             }
         done
     return
@@ -60,80 +60,44 @@ checkEnvironment() {
         exit 1;
     fi
 
+    build_dir=$zoomaHome/index/virtuoso;
     port=$(loadProperty "virtuoso.builder.port");
+    httpport=$(loadProperty "virtuoso.builder.httpport");
     threads=$(loadProperty "virtuoso.builder.threads");
 
-    build_dir=$zoomaHome/index/virtuoso;
-}
-
-createCleanInstance() {
-    local templatefile=$base/templates/template-config.ini
-
-    if [ ! -e $templatefile ] ; then
-	    echo `date` "Missing template file: $templatefile";
-	    exit 2;
-    fi
-
-    if [ -f $build_dir/db/virtuoso.lck ] ; then
-        echo "Stopping virtuoso";
-        stopVirtuoso;
-    fi
-
-    echo `date` "Creating clean instance for $build_dir"
-    rm -rf $build_dir || exit 3;
-    mkdir -p $build_dir || exit 3;
-    mkdir -p $build_dir/db || exit 3;
-
-    configfile=$build_dir/db/virtuoso.ini
-
-    echo `date` "Generating config file"
-
-    (cat $templatefile | sed  -e "s#\$VIRTUOSO_HOME#$VIRTUOSO_HOME#g" -e "s#\$DBDIR#$build_dir#g" -e "s#\$SERVERPORT#$port#g" -e "s#\$HTTPPORT#$httpport#g" > $configfile) || exit 3
-
-    startVirtuoso;
-
-    echo `date` "Enabling federated queries"
-    $VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba $base/templates/enable-federated.sql > /dev/null || exit $?
-
-    echo `date` "Removing default Virtuoso SPARQL description graph"
-    $VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba $base/templates/remove-sparqldesc.sql >/dev/null || exit $?
-
-    #echo `date` "Enabling Virtuoso text index"
-    #$VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba $base/templates/enable-text-index.sql >/dev/null || exit $?
-
-    echo `date` "Done"
+    echo "A new Virtuoso instance will be created in $build_dir and will run on port $port. Loading is enabled with $threads parallel processes.";
 }
 
 startVirtuoso() {
     $VIRTUOSO_HOME/bin/virtuoso-t -c  $build_dir/db/virtuoso.ini || exit 3
 
-    echo `date` "Virtuoso started, waiting for ready state"
+    echo "Starting Virtuoso on port $port, waiting for ready state...";
 
     i=0
     status=1
     while [ $status -ne 0 ] ; do
 	    i=`expr $i + 1`
 	    if test $i -gt 300 ; then
-		    echo `date` "Virtuoso is not ready after waiting 5 minutes"
+		    echo "Virtuoso is not ready after waiting 5 minutes"
 		    exit 4
 	    fi
 	    sleep 1
         if [ ! -f $build_dir/db/virtuoso.lck ] ; then
-		    echo `date` "Failed to start Virtuoso"
+		    echo "Failed to start Virtuoso"
 		    exit 4
 	    fi
 	    $VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba exec="status();" &> /dev/null
 	    status=$?
     done
 
-    echo `date` "Virtuoso ready on ports $port"
+    echo "Virtuoso ready on ports $port"
 }
 
 stopVirtuoso() {
     lockfile=$build_dir/db/virtuoso.lck
 
     if [ ! -f $lockfile ] ; then
-        echo `date` "Virtuoso not running"
+        echo "Virtuoso not running"
         exit 0
     fi
 
@@ -141,16 +105,16 @@ stopVirtuoso() {
     pid=${tmp#VIRT_PID=};
 
     if [ ! $pid ] ; then
-        echo `date` "Unable to parse Virtuoso process ID"
+        echo "Unable to parse Virtuoso process ID"
         exit 2
     fi
 
-    echo `date` "Stopping Virtuoso process $pid"
+    echo "Stopping Virtuoso process $pid"
 
     kill -2 $pid
 
     if test $? -ne 0 ; then
-        echo `date` "Unable to stop Virtuoso"
+        echo "Unable to stop Virtuoso"
         exit 3
     fi
 
@@ -159,12 +123,56 @@ stopVirtuoso() {
         sleep 1
         i=`expr $i + 1`
         if test $i -gt 20 ; then
-            echo `date` "Virtuoso has not shut down after waiting 20 seconds"
+            echo "Virtuoso has not shut down after waiting 20 seconds"
             exit 4
         fi
     done
 }
 
+die() {
+    stopVirtuoso;
+    exit $1;
+}
+
+createCleanInstance() {
+    local templates=$base/virtuoso-control/templates;
+    local templatefile=$templates/template-config.ini
+
+    if [ ! -e $templatefile ] ; then
+	    echo "Missing template file: $templatefile";
+	    exit 2;
+    fi
+
+    if [ -f $build_dir/db/virtuoso.lck ] ; then
+        echo "Stopping virtuoso";
+        stopVirtuoso;
+    fi
+
+    echo "Creating clean Virtuoso instance in $build_dir";
+    rm -rf $build_dir || exit 3;
+    mkdir -p $build_dir || exit 3;
+    mkdir -p $build_dir/db || exit 3;
+    mkdir -p $build_dir/log || exit 3;
+
+    configfile=$build_dir/db/virtuoso.ini
+
+    echo "Generating Virtuoso config file at $configfile";
+
+    (cat $templatefile | sed  -e "s#\$VIRTUOSO_HOME#$VIRTUOSO_HOME#g" -e "s#\$DBDIR#$build_dir#g" -e "s#\$SERVERPORT#$port#g" -e "s#\$HTTPPORT#$httpport#g" -e "s#\$ZOOMA_HOME#$zoomaHome#g" > $configfile) || exit 3
+
+    startVirtuoso;
+
+    echo "Enabling federated queries"
+    $VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba $templates/enable-federated.sql &>> $build_dir/log/virtuoso-zooma.log || die $?
+
+    echo "Removing default Virtuoso SPARQL description graph"
+    $VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba $templates/remove-sparqldesc.sql &>> $build_dir/log/virtuoso-zooma.log || die $?
+
+    #echo "Enabling Virtuoso text index"
+    #$VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba $templates/enable-text-index.sql &>> $build_dir/log/virtuoso-zooma.log || die $?
+
+    echo "Successfully created clean Virtuoso instance";
+}
 
 # main body of script
 
@@ -188,30 +196,30 @@ createCleanInstance;
 # now run commands to load zooma data into local virtuoso
 loadfiles="ld_dir_all('$rdf_dir', '*.rdf', 'http://rdf.ebi.ac.uk/dataset/zooma');"
 
-$VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba exec="$loadfiles" || exit 4;
+$VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba exec="$loadfiles" &>> $build_dir/log/virtuoso-zooma.log || die 4;
 
 loadfiles="ld_dir_all('$rdf_dir', '*.owl', 'http://rdf.ebi.ac.uk/dataset/zooma');"
 
-$VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba exec="$loadfiles" || exit 4;
+$VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba exec="$loadfiles" &>> $build_dir/log/virtuoso-zooma.log || die 4;
 
 echo "Finished setting files to load, starting loader..."
 
 if [ $threads ] ; then
     # We want 1 fewer loader than the number of CPUs
-    echo `date` "Starting $(($threads-1)) loader processes"
+    echo "Starting $(($threads-1)) loader processes"
     for ((i=1; i<$threads; i++)); do
-        $VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba exec="rdf_loader_run();" &
+        $VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba exec="rdf_loader_run();" &>> $build_dir/log/virtuoso-zooma.log &
     done
-    echo `date` "Waiting for loaders"
+    echo "Waiting for loaders"
     wait
 else
-    $VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba exec="rdf_loader_run();"
+    $VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba exec="rdf_loader_run();" &>> $build_dir/log/virtuoso-zooma.log
 fi
 
 
-echo `date` "Creating checkpoint"
+echo "Creating checkpoint"
 
-$VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba exec="checkpoint;"
+$VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba exec="checkpoint;" &>> $build_dir/log/virtuoso-zooma.log
 
 echo "finished indexing files, executing final virtuoso configuration scripts..."
 
@@ -220,21 +228,20 @@ echo "creating inference rules set"
 # setting inference rules
 loadrules="rdfs_rule_set ('default-rules', 'http://rdf.ebi.ac.uk/dataset/zooma')"
 
-$VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba exec="$loadrules" || exit 4;
+$VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba exec="$loadrules" &>> $build_dir/log/virtuoso-zooma.log || die 4;
 
-echo `date` "Updating VoID graph with  number of triples, SPARQL description"
+echo "Updating VoID graph with  number of triples, SPARQL description"
 
 templatefile=$base/virtuoso-control/templates/update-provenance-template.sql
-$VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba $templatefile || exit $?
+$VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba $templatefile &>> $build_dir/log/virtuoso-zooma.log || die $?
 
-echo `date` "Creating checkpoint"
-$VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba exec="checkpoint;"
+echo "Creating checkpoint"
+$VIRTUOSO_HOME/bin/isql 127.0.0.1:$port dba dba exec="checkpoint;" &>> $build_dir/log/virtuoso-zooma.log || die $?
 
-echo `date` "Data loading finished"
+echo "Data loading finished"
+
 # finished data loading, stop virtuoso now we've built all our indexes
-
 stopVirtuoso;
 
-echo `date` "Virtuoso indexes have been built and saved in $build_dir";
-echo `date` "Done.";
-
+echo "Virtuoso indexes have been built and saved in $build_dir";
+echo "Done.";
