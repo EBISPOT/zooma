c31823bc9a711547a12a8b294724f4df2817de52
Tony Burdett <tburdett@ebi.ac.uk>
2017-01-06 11:26:49 +0000

    setting up message publishing for annotations saved in mongo

---
+++ zooma-mongo/src/main/java/uk/ac/ebi/spot/zooma/messaging/Constants.java
@@ -0,0 +1,17 @@
+package uk.ac.ebi.spot.zooma.messaging;
+
+/**
+ * Javadocs go here!
+ *
+ * @author Tony Burdett
+ * @date 06/01/17
+ */
+public class Constants {
+    public class Queues {
+        public static final String ANNOTATION_SAVE = "annotation.save.queue";
+    }
+
+    public class Exchanges {
+        public static final String ANNOTATION_FANOUT_EXCHANGE = "annotation.fanout.exchange";
+    }
+}
---
+++ zooma-mongo/src/main/java/uk/ac/ebi/spot/zooma/messaging/QueueConfig.java
@@ -0,0 +1,21 @@
+package uk.ac.ebi.spot.zooma.messaging;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Javadocs go here!
+ *
+ * @author Tony Burdett
+ * @date 06/01/17
+ */
+@Configuration
+public class QueueConfig {
+    @Bean Queue queue() { return new Queue(Constants.Queues.ANNOTATION_SAVE, false); }
+    @Bean Binding binding(Queue queue, FanoutExchange exchange) { return BindingBuilder.bind(queue).to(exchange); }
+    @Bean FanoutExchange exchange() { return new FanoutExchange(Constants.Exchanges.ANNOTATION_FANOUT_EXCHANGE);}
+}
