e73a9131fff6729340ceb31f005c4de4a18cdf72
Tony Burdett <tburdett@ebi.ac.uk>
2017-02-16 13:40:52 +0000

    more pom tidy up and using annotation serialization in receiver

--- zooma-annotation-consumer/src/main/java/uk/ac/ebi/spot/zooma/messaging/AnnotationSubmissionReceiver.java
+++ zooma-annotation-consumer/src/main/java/uk/ac/ebi/spot/zooma/messaging/AnnotationSubmissionReceiver.java
@@ -2,6 +2,7 @@ package uk.ac.ebi.spot.zooma.messaging;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+import uk.ac.ebi.spot.zooma.model.Annotation;
 
 /**
  * Javadocs go here!
@@ -12,7 +13,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class AnnotationSubmissionReceiver {
     @RabbitListener(queues = "annotation.save.queue")
-    public void handleAnnotationSubmission(String message) {
-        System.out.println("We have detected that a new annotation was submitted! " + message);
+    public void handleAnnotationSubmission(Annotation annotation) {
+        System.out.println("We have detected that a new annotation was submitted! " + annotation);
     }
 }
