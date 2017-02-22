6bae8d1d01e2c7b77f50ebc756bd3e44330acd72
olgavrou <olgavrou@gmail.com>
2017-02-22 17:54:59 +0000

    disabled zooma-ui for now

--- zooma-ui/src/main/java/uk/ac/ebi/spot/zooma/config/UIConfig.java
+++ zooma-ui/src/main/java/uk/ac/ebi/spot/zooma/config/UIConfig.java
@@ -1,30 +1,30 @@
-package uk.ac.ebi.spot.zooma.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.web.client.RestTemplate;
-
-/**
- * Created by olgavrou on 04/11/2016.
- */
-@Configuration
-public class UIConfig {
-
-    @Bean
-    RestTemplate getRestTemplate(){
-        return new RestTemplate();
-    }
-
-    @Bean
-    public TaskExecutor taskExecutor(){
-        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
-        threadPoolTaskExecutor.setCorePoolSize(25);
-        threadPoolTaskExecutor.setMaxPoolSize(25);
-        threadPoolTaskExecutor.setQueueCapacity(25);
-        threadPoolTaskExecutor.setWaitForTasksToCompleteOnShutdown(true);
-
-        return threadPoolTaskExecutor;
-    }
-}
+//package uk.ac.ebi.spot.zooma.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.task.TaskExecutor;
+//import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+//import org.springframework.web.client.RestTemplate;
+//
+///**
+// * Created by olgavrou on 04/11/2016.
+// */
+//@Configuration
+//public class UIConfig {
+//
+//    @Bean
+//    RestTemplate getRestTemplate(){
+//        return new RestTemplate();
+//    }
+//
+//    @Bean
+//    public TaskExecutor taskExecutor(){
+//        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+//        threadPoolTaskExecutor.setCorePoolSize(25);
+//        threadPoolTaskExecutor.setMaxPoolSize(25);
+//        threadPoolTaskExecutor.setQueueCapacity(25);
+//        threadPoolTaskExecutor.setWaitForTasksToCompleteOnShutdown(true);
+//
+//        return threadPoolTaskExecutor;
+//    }
+//}
--- zooma-ui/src/main/java/uk/ac/ebi/spot/zooma/model/AnnotationProperty.java
+++ zooma-ui/src/main/java/uk/ac/ebi/spot/zooma/model/AnnotationProperty.java
@@ -1,48 +1,48 @@
-package uk.ac.ebi.spot.zooma.model;
-
-
-import uk.ac.ebi.spot.zooma.model.mongo.Property;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-/**
- * Created by olgavrou on 25/10/2016.
- */
-public class AnnotationProperty {
-
-    private String text;
-
-    private List<Property> properties;
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public List<Property> getProperties() {
-        properties = new ArrayList<>();
-        ArrayList<String> textLines = new ArrayList<>(Arrays.asList(text.split("\r\n")));
-
-        for (String textLine : textLines) {
-            String[] line = textLine.split("\t");
-            if (line.length > 0) {
-                String propertyValue = line[0];
-                if (line.length > 1) {
-                    String propertyType = line[1];
-                    Property typedProperty = new Property(propertyType, propertyValue);
-                    properties.add(typedProperty);
-                } else {
-                    Property untypedProperty = new UntypedProperty(propertyValue);
-                    properties.add(untypedProperty);
-                }
-            }
-        }
-        return properties;
-    }
-
-}
+//package uk.ac.ebi.spot.zooma.model;
+//
+//
+//import uk.ac.ebi.spot.zooma.model.mongo.Property;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//
+///**
+// * Created by olgavrou on 25/10/2016.
+// */
+//public class AnnotationProperty {
+//
+//    private String text;
+//
+//    private List<Property> properties;
+//
+//    public String getText() {
+//        return text;
+//    }
+//
+//    public void setText(String text) {
+//        this.text = text;
+//    }
+//
+//    public List<Property> getProperties() {
+//        properties = new ArrayList<>();
+//        ArrayList<String> textLines = new ArrayList<>(Arrays.asList(text.split("\r\n")));
+//
+//        for (String textLine : textLines) {
+//            String[] line = textLine.split("\t");
+//            if (line.length > 0) {
+//                String propertyValue = line[0];
+//                if (line.length > 1) {
+//                    String propertyType = line[1];
+//                    Property typedProperty = new Property(propertyType, propertyValue);
+//                    properties.add(typedProperty);
+//                } else {
+//                    Property untypedProperty = new Property("", propertyValue);
+//                    properties.add(untypedProperty);
+//                }
+//            }
+//        }
+//        return properties;
+//    }
+//
+//}
