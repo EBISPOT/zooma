dd1cf9c2245ef96a5eafeeb79000b04161c8adc0
Warren Read <wread@ebi.ac.uk>
2018-09-20 12:21:09 +0100

    Set Neo4j client username and password to null, explicitly

--- start_zooma_neo4j.sh
+++ start_zooma_neo4j.sh
@@ -1,8 +1,14 @@
+#!/bin/sh
+
+#  You _must_ enclose commented continuation lines in backticks, but not
+#  the line continuation marks themselves!
+
 java -jar \
      -Dspring.rabbitmq.host=rabbitmq \
      -Dspring.rabbitmq.activate=true \
      -Dspring.data.neo4j.uri=http://neo4j:7474 \
-     -Dspring.data.neo4j.username=neo4j \
+     `# -Dspring.data.neo4j.username=neo4j` \
+     -Dspring.data.neo4j.username=null \
      -Dspring.data.neo4j.password=null \
      -Dspring.data.neo4j.indexes.auto=assert \
      -Dspring.jackson.serialization.write-dates-as-timestamps=false \
