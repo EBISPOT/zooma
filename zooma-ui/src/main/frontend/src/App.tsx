4d01c2065684fa72e8d2d36b4f7179d9b869e46c
James McLaughlin <james@mclgh.net>
2021-02-26 10:12:47 +0000

    add survey banner

--- zooma-ui/src/main/frontend/src/App.tsx
+++ zooma-ui/src/main/frontend/src/App.tsx
@@ -14,6 +14,17 @@ export default function App() {
     <div className="App">
       <EBIMasthead/>
       <Navbar />
+
+      <div style={{width: '100%', backgroundColor: '#00d9ff', padding: '30px', textAlign: 'center', fontSize: 'large'}}>
+        <p style={{color: 'black'}}>
+          If you’ve ever found our data helpful, please take our impact survey (15 min). Your replies will help keep the data flowing to the scientific community.
+        </p>
+        <br />
+        <a style={{display: 'inline-block', cursor: 'pointer', outline: 'none', border: 'none'}} href="https://www.surveymonkey.co.uk/r/EMBL-EBI_Impact_DR">
+          <span style={{color: 'black', background: 'none', border: '1px solid black', borderRadius: '8px', padding: '12px'}}>Take Survey</span>
+        </a>
+      </div>
+
       <Route exact path='/' component={Home}></Route>
       <Route exact path='/docs' component={Docs}></Route>
       <Route exact path='/docs/search' component={DocsSearch}></Route>
